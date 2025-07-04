8ef215fcb19e07aedf6d1b93902d4de42b5b3a9d
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-07-04 15:07:27 +0530

    added verifyUser and verifyAdmin middleware

---
+++ backend/middlewares/verifyAdmin.js
@@ -0,0 +1,23 @@
+
+
+import Admin from "../models/admin.model.js";
+
+
+const verifyAdmin = async (req, res, next) => {
+    try {
+        const user = req.user;
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const admin = await Admin.findOne({ admin: user._id });
+        if (!admin) {
+            return res.status(403).json({ message: "Access denied, not an admin" });
+        }
+        next();
+    } catch (error) {
+        console.error("Error in verifyAdmin middleware:", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export default verifyAdmin;
---
+++ backend/middlewares/verifyUser.js
@@ -0,0 +1,39 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token =
+      authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : req.cookies?.accessToken || null;
+
+    if (!token) {
+      return res.status(400).json({ message: "token not provided" });
+    }
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const user = await User.findOne({
+      _id: decodedToken._id,
+      isEmailVerified: true,
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found or email not verified" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in verifyUser middleware:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default verifyUser;
