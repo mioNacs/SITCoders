b3023814bf0a5201c989380187128d971d414350
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-08-17 23:42:45 +0530

    fix a minor bug

--- backend/utilities/transporter.js
+++ backend/utilities/transporter.js
@@ -4,8 +4,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: process.env.SMTP_HOST,
-  port: smtpPort,
-  secure: smtpPort === 465, // true for 465, false for 587/25
+  port: parseInt(process.env.SMTP_PORT),
+  secure: true, // true for 465, false for 587/25
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
