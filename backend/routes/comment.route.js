fbd9b9d6c7d1c4d2faa708ee36ce386f28b86e11
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-08-19 15:20:07 +0530

    all the comments have been deleted

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import isVarifiedByAdmin from "../middlewares/isVarifiedByAdmin.js";
 import verifyAdmin from "../middlewares/verifyAdmin.js";
+import Comment from "../models/comment.model.js";
 import {
   createComment,
   createReply,
@@ -11,6 +12,12 @@ import {
 } from "../controllers/comment.controller.js";
 
 const router = Router();
+
+// This is the route i used to delete all the comments 
+// router.get("/delete-comment",async(req,res) => {
+//   await Comment.deleteMany({});
+//   res.send("All comments deleted");
+// })
 
 router.post("/create/:postId", isVarifiedByAdmin, createComment);
 router.post("/reply/:commentId", isVarifiedByAdmin, createReply);
