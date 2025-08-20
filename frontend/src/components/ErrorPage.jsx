5180c535214ea05b32c78d401629e1724aaa8de2
Navneet Raj <Navneet78030@gmail.com>
2025-08-20 18:40:33 +0530

    minor fix

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -13,7 +13,7 @@ function ErrorPage() {
             {error.statusText || error.message}
           </pre>
         )}
-        <Link to="/" className="text-orange-500 hover:text-orange-600 font-medium">
+        <Link to="/login" className="text-orange-500 hover:text-orange-600 font-medium">
           Go back to Home
         </Link>
       </div>
